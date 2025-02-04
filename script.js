f3931ad3ac70523f9201523cfb45ebdd50bcb3b6
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-02-04 12:27:48 -0300

    update portfólio

---
+++ script.js
@@ -0,0 +1,18 @@
+function inicioScrollSuave() {
+	let linksInt = document.querySelectorAll('.scrolling a[href^="#"]')
+
+	function smoothScrolling(event) {
+		event.preventDefault()
+		let href = event.currentTarget.getAttribute('href')
+		let section = document.querySelector(href)
+		section.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	}
+
+	linksInt.forEach(link => {
+		link.addEventListener('click', smoothScrolling)
+	})
+}
+inicioScrollSuave()
