e8cfe9edfce3aa155a5da6e999d102ffe5694216
Juliano Viégas Reis <114380427+JulianoVReis@users.noreply.github.com>
2025-02-25 13:07:19 -0300

    update portfolio

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function scrollSuave() {
 	})
 }
 scrollSuave()
+
+function typeWriter() {
+	new TypeIt("#texto01", {
+		strings: 'Esta é a seção <span class="span-verde">Home.</span>',
+		speed: 50,
+		loop: false,
+		waitUntilVisible: true
+	}).go()
+
+	new TypeIt("#texto02", {
+		strings: 'Esta é a seção <span class="span-verde">Sobre</span>, onde falo um pouco sobre mim e meus objetivos.',
+		speed: 50,
+		loop: false,
+		waitUntilVisible: true
+	}).go()
+
+	new TypeIt("#texto03", {
+		strings: 'Esta é a seção de <span class="span-verde">Skills</span>, que contém meus conhecimentos técnicos.',
+		speed: 50,
+		loop: false,
+		waitUntilVisible: true
+	}).go()
+
+	new TypeIt("#texto04", {
+		strings: 'Esta é a seção dos <span class="span-verde">Projetos</span> nos quais trabalhei.',
+		speed: 50,
+		loop: false,
+		waitUntilVisible: true
+	}).go()
+}
+typeWriter()
