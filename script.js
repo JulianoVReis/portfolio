fda2036dd7df363a9112c5caddf972e9d8334664
Juliano Reis <julianovreis@gmail.com>
2025-04-10 10:43:04 -0300

    melhorias

--- script.js
+++ script.js
@@ -19,28 +19,28 @@ scrollSuave()
 
 function typeWriter() {
 	new TypeIt("#texto01", {
-		strings: 'Esta é a seção <span class="span-verde">Home.</span>',
+		strings: 'Essa é a seção <span class="span-verde">Home.</span>',
 		speed: 50,
 		loop: false,
 		waitUntilVisible: true
 	}).go()
 
 	new TypeIt("#texto02", {
-		strings: 'Esta é a seção <span class="span-verde">Sobre</span>, onde falo um pouco sobre mim e meus objetivos.',
+		strings: 'Essa é a seção <span class="span-verde">Sobre</span>, onde falo um pouco sobre mim e meus objetivos.',
 		speed: 50,
 		loop: false,
 		waitUntilVisible: true
 	}).go()
 
 	new TypeIt("#texto03", {
-		strings: 'Esta é a seção de <span class="span-verde">Skills</span>, que contém meus conhecimentos técnicos.',
+		strings: 'Essa é a seção de <span class="span-verde">Skills</span>, que contém meus conhecimentos técnicos.',
 		speed: 50,
 		loop: false,
 		waitUntilVisible: true
 	}).go()
 
 	new TypeIt("#texto04", {
-		strings: 'Esta é a seção dos <span class="span-verde">Projetos</span> nos quais trabalhei.',
+		strings: 'Essa é a seção dos <span class="span-verde">Projetos</span> nos quais trabalhei.',
 		speed: 50,
 		loop: false,
 		waitUntilVisible: true
