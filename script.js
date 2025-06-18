977622f6c5c78d6f89be70aeb51444638a637041
Juliano Reis <julianovreis@gmail.com>
2025-06-18 11:29:40 -0300

    update portfolio

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 function scrollSuave() {
-	let linksInt = document.querySelectorAll('.scrolling a[href^="#"]')
+	let linksInt = document.querySelectorAll('.scrolling a[href^="#"]');
 
 	function smoothScrolling(event) {
-		event.preventDefault()
-		let href = event.currentTarget.getAttribute('href')
-		let section = document.querySelector(href)
+		event.preventDefault();
+		let href = event.currentTarget.getAttribute("href");
+		let section = document.querySelector(href);
 		section.scrollIntoView({
-			behavior: 'smooth',
-			block: 'start',
-		})
+			behavior: "smooth",
+			block: "start",
+		});
 	}
 
-	linksInt.forEach(link => {
-		link.addEventListener('click', smoothScrolling)
-	})
+	linksInt.forEach((link) => {
+		link.addEventListener("click", smoothScrolling);
+	});
 }
-scrollSuave()
+scrollSuave();
 
 function typeWriter() {
 	new TypeIt("#texto01", {
-		strings: 'Essa é a seção <span class="span-verde">Home.</span>',
+		strings: 'Esta é a seção <span class="span-verde">Home.</span>',
 		speed: 50,
 		loop: false,
-		waitUntilVisible: true
-	}).go()
+		waitUntilVisible: true,
+	}).go();
 
 	new TypeIt("#texto02", {
-		strings: 'Essa é a seção <span class="span-verde">Sobre</span>, onde falo um pouco sobre mim e meus objetivos.',
+		strings: 'Esta é a seção <span class="span-verde">Sobre</span>, onde falo um pouco sobre mim e meus objetivos.',
 		speed: 50,
 		loop: false,
-		waitUntilVisible: true
-	}).go()
+		waitUntilVisible: true,
+	}).go();
 
 	new TypeIt("#texto03", {
-		strings: 'Essa é a seção de <span class="span-verde">Skills</span>, que contém meus conhecimentos técnicos.',
+		strings: 'Esta é a seção de <span class="span-verde">Skills</span>, que contém meus conhecimentos técnicos.',
 		speed: 50,
 		loop: false,
-		waitUntilVisible: true
-	}).go()
+		waitUntilVisible: true,
+	}).go();
 
 	new TypeIt("#texto04", {
-		strings: 'Essa é a seção dos <span class="span-verde">Projetos</span> nos quais trabalhei.',
+		strings: 'Esta é a seção dos <span class="span-verde">Projetos</span> nos quais trabalhei.',
 		speed: 50,
 		loop: false,
-		waitUntilVisible: true
-	}).go()
+		waitUntilVisible: true,
+	}).go();
 }
-typeWriter()
+typeWriter();
